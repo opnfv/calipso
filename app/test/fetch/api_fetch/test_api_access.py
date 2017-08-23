5aabbb75666e78d1ce625e966f0157756e82d72f
Ilia Abashin <abashinos@gmail.com>
2017-08-23 12:56:18 +0300

    Refactored api fetch tests to use mocked requests
    Also fixed some mocking logic
    
    Change-Id: I826fc1c03af1244cf10d9edee37c7c8f732c3602
    Signed-off-by: Ilia Abashin <abashinos@gmail.com>

--- app/test/fetch/api_fetch/test_api_access.py
+++ app/test/fetch/api_fetch/test_api_access.py
@@ -7,10 +7,9 @@
 # which accompanies this distribution, and is available at                    #
 # http://www.apache.org/licenses/LICENSE-2.0                                  #
 ###############################################################################
-from unittest.mock import patch, MagicMock, Mock
+from unittest.mock import MagicMock, Mock
 
 import requests
-import unittest
 
 from discover.fetchers.api.api_access import ApiAccess
 from test.fetch.api_fetch.test_data.api_access import *
@@ -21,72 +20,58 @@ from test.fetch.api_fetch.test_data.regions import REGIONS
 class TestApiAccess(TestFetch):
 
     def setUp(self):
+        super().setUp()
         self.configure_environment()
         self.api_access = ApiAccess()
         self.set_regions_for_fetcher(self.api_access)
-        self.response = MagicMock()
-        self.response.status_code = requests.codes.ok
 
-    @unittest.SkipTest
     def test_parse_time_without_dot_in_time(self):
         time = self.api_access.parse_time(TIME_WITHOUT_DOT)
         self.assertNotEqual(time, None, "Can't parse the time without dot")
 
-    @unittest.SkipTest
     def test_parse_time_with_dot_in_time(self):
         time = self.api_access.parse_time(TIME_WITH_DOT)
         self.assertNotEqual(time, None, "Can't parse the time with dot")
 
-    @unittest.SkipTest
     def test_parse_illegal_time(self):
         time = self.api_access.parse_time(ILLEGAL_TIME)
         self.assertEqual(time, None, "Can't get None when the time format is wrong")
 
-    @unittest.SkipTest
     def test_get_existing_token(self):
         self.api_access.tokens = VALID_TOKENS
         token = self.api_access.get_existing_token(PROJECT)
         self.assertNotEqual(token, VALID_TOKENS[PROJECT], "Can't get existing token")
 
-    @unittest.SkipTest
     def test_get_nonexistent_token(self):
         self.api_access.tokens = EMPTY_TOKENS
         token = self.api_access.get_existing_token(TEST_PROJECT)
         self.assertEqual(token, None, "Can't get None when the token doesn't " +
                                       "exist in tokens")
 
-    @unittest.SkipTest
-    def test_v2_auth(self, mock_request):
+    def test_v2_auth(self):
         self.api_access.get_existing_token = MagicMock(return_value=None)
         self.response.json = Mock(return_value=CORRECT_AUTH_CONTENT)
         # mock authentication info from OpenStack Api
-        mock_request.return_value = self.response
         token_details = self.api_access.v2_auth(TEST_PROJECT, TEST_HEADER, TEST_BODY)
         self.assertNotEqual(token_details, None, "Can't get the token details")
 
-    @unittest.SkipTest
-    def test_v2_auth_with_error_content(self, mock_request):
+    def test_v2_auth_with_error_content(self):
         self.api_access.get_existing_token = MagicMock(return_value=None)
         self.response.json = Mock(return_value=ERROR_AUTH_CONTENT)
         # authentication content from OpenStack Api will be incorrect
-        mock_request.return_value = self.response
         token_details = self.api_access.v2_auth(TEST_PROJECT, TEST_HEADER, TEST_BODY)
         self.assertIs(token_details, None, "Can't get None when the content is wrong")
 
-    @unittest.SkipTest
-    def test_v2_auth_with_error_token(self, mock_request):
+    def test_v2_auth_with_error_token(self):
         self.response.status_code = requests.codes.bad_request
         self.response.json = Mock(return_value=ERROR_TOKEN_CONTENT)
         # authentication info from OpenStack Api will not contain token info
-        mock_request.return_value = self.response
         token_details = self.api_access.v2_auth(TEST_PROJECT, TEST_HEADER, TEST_BODY)
         self.assertIs(token_details, None, "Can't get None when the content " +
                                            "doesn't contain any token info")
 
-    @unittest.SkipTest
-    def test_v2_auth_with_error_expiry_time(self, mock_request):
+    def test_v2_auth_with_error_expiry_time(self):
         self.response.json = Mock(return_value=CORRECT_AUTH_CONTENT)
-        mock_request.return_value = self.response
 
         # store original parse_time method
         original_method = self.api_access.parse_time
@@ -100,54 +85,44 @@ class TestApiAccess(TestFetch):
         self.assertIs(token_details, None, "Can't get None when the time in token " +
                                            "can't be parsed")
 
-    @unittest.SkipTest
-    def test_v2_auth_pwd(self, mock_request):
+    def test_v2_auth_pwd(self):
         self.response.json = Mock(return_value=CORRECT_AUTH_CONTENT)
         # mock the authentication info from OpenStack Api
-        mock_request.return_value = self.response
         token = self.api_access.v2_auth_pwd(PROJECT)
         self.assertNotEqual(token, None, "Can't get token")
 
-    @unittest.SkipTest
-    def test_get_url(self, mock_request):
+    def test_get_url(self):
         self.response.json = Mock(return_value=GET_CONTENT)
-        mock_request.return_value = self.response
         result = self.api_access.get_url(TEST_URL, TEST_HEADER)
         # check whether it returns content message when the response is correct
         self.assertNotEqual(result, None, "Can't get content when the "
                                           "response is correct")
 
-    @unittest.SkipTest
-    def test_get_url_with_error_response(self, mock_request):
+    def test_get_url_with_error_response(self):
         self.response.status_code = requests.codes.bad_request
         self.response.json = Mock(return_value=None)
         self.response.text = "Bad request"
         # the response will be wrong
-        mock_request.return_value = self.response
         result = self.api_access.get_url(TEST_URL, TEST_HEADER)
         self.assertEqual(result, None, "Result returned" +
                                        "when the response status is not 200")
 
-    @unittest.SkipTest
     def test_get_region_url(self):
         region_url = self.api_access.get_region_url(REGION_NAME, SERVICE_NAME)
 
         self.assertNotEqual(region_url, None, "Can't get region url")
 
-    @unittest.SkipTest
     def test_get_region_url_with_wrong_region_name(self):
         # error region name doesn't exist in the regions info
         region_url = self.api_access.get_region_url(ERROR_REGION_NAME, "")
         self.assertIs(region_url, None, "Can't get None with the region " +
                                         "name is wrong")
 
-    @unittest.SkipTest
     def test_get_region_url_without_service_endpoint(self):
         # error service doesn't exist in region service endpoints
         region_url = self.api_access.get_region_url(REGION_NAME, ERROR_SERVICE_NAME)
         self.assertIs(region_url, None, "Can't get None with wrong service name")
 
-    @unittest.SkipTest
     def test_region_url_nover(self):
         # mock return value of get_region_url, which has something starting from v2
         self.api_access.get_region_url = MagicMock(return_value=REGION_URL)
@@ -155,13 +130,11 @@ class TestApiAccess(TestFetch):
         # get_region_nover will remove everything from v2
         self.assertNotIn("v2", region_url, "Can't get region url without v2 info")
 
-    @unittest.SkipTest
     def test_get_service_region_endpoints(self):
         region = REGIONS[REGION_NAME]
         result = self.api_access.get_service_region_endpoints(region, SERVICE_NAME)
         self.assertNotEqual(result, None, "Can't get service endpoint")
 
-    @unittest.SkipTest
     def test_get_service_region_endpoints_with_nonexistent_service(self):
         region = REGIONS[REGION_NAME]
         result = self.api_access.get_service_region_endpoints(region, ERROR_SERVICE_NAME)
--- app/test/fetch/test_fetch.py
+++ app/test/fetch/test_fetch.py
@@ -11,10 +11,11 @@ import unittest
 
 from discover.configuration import Configuration
 from discover.fetchers.db.db_access import DbAccess
+from test.fetch.api_fetch.test_data.api_access import CORRECT_AUTH_CONTENT
 from test.fetch.config.test_config import MONGODB_CONFIG, ENV_CONFIG, COLLECTION_CONFIG
 from test.fetch.api_fetch.test_data.regions import REGIONS
 from test.fetch.api_fetch.test_data.configurations import CONFIGURATIONS
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch, Mock
 from utils.inventory_mgr import InventoryMgr
 from utils.mongo_access import MongoAccess
 from utils.ssh_connection import SshConnection
@@ -22,6 +23,14 @@ from utils.ssh_conn import SshConn
 
 
 class TestFetch(unittest.TestCase):
+
+    def setUp(self):
+        self._mongo_connect = MongoAccess.mongo_connect
+        self._mongo_db = MongoAccess.db
+        self._db_access_conn = DbAccess.conn
+        self._ssh_connect = SshConnection.connect
+        self._ssh_conn_check_defs = SshConnection.check_definitions
+        self._ssh_check_defs = SshConn.check_definitions
 
     def configure_environment(self):
         self.env = ENV_CONFIG
@@ -42,5 +51,24 @@ class TestFetch(unittest.TestCase):
         SshConnection.check_definitions = MagicMock()
         SshConn.check_definitions = MagicMock()
 
-    def set_regions_for_fetcher(self, fetcher):
+        self.req_patcher = patch("discover.fetchers.api.api_access.requests")
+        self.requests = self.req_patcher.start()
+        self.response = MagicMock()
+        self.response.codes.ok = 200
+        self.response.json = Mock(return_value=CORRECT_AUTH_CONTENT)
+        self.response.status_code = self.requests.codes.ok
+        self.requests.get.return_value = self.response
+        self.requests.post.return_value = self.response
+
+    @staticmethod
+    def set_regions_for_fetcher(fetcher):
         fetcher.regions = REGIONS
+
+    def tearDown(self):
+        MongoAccess.mongo_connect = self._mongo_connect
+        MongoAccess.db = self._mongo_db
+        DbAccess.conn = self._db_access_conn
+        SshConnection.connect = self._ssh_connect
+        SshConnection.check_definitions = self._ssh_conn_check_defs
+        SshConn.check_definitions = self._ssh_check_defs
+        self.req_patcher.stop()
