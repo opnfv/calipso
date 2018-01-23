8ace6ba873abfec7a90b9e5e8384b3543a773025
Yaron Yogev <yaronyogev@gmail.com>
2018-01-23 15:26:21 +0200

    revert some docs changes from last commit by Koren
    table fixes by Aric Gardner in docs/release/admin-guide.rst
      were accidentaly destroyed
    files were accidentaly removed: docs/index.rst, docs/release/index.rst
    
    Change-Id: I9f826f524efd3e1af596055f0f16433551a918eb
    Signed-off-by: Yaron Yogev <yaronyogev@gmail.com>

--- app/test/verify.sh
+++ app/test/verify.sh
@@ -11,6 +11,14 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum update
+sudo yum -y install python35
+sudo pip install virtualenv
+virtualenv -p $(which python3.5) $WORKSPACE/venv       
+. $WORKSPACE/venv/bin/activate
+
 PYTHONPATH=$PWD/app python3 -m unittest discover -s app/test/api
 PYTHONPATH=$PWD/app python3 -m unittest discover -s app/test/event_based_scan
 PYTHONPATH=$PWD/app python3 -m unittest discover -s app/test/fetch
